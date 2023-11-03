879369fb585037d9297c104656db3bef0f2cddc6
rocsmytune <smz0705@163.com>
2023-11-03 22:48:42 +0800

    #add: implemented feignClient methods

---
+++ chaosoj-backend-judge-service/src/main/java/com/rocs/chaosojbackendjudgeservice/controller/inner/JudgeInnerController.java
@@ -0,0 +1,19 @@
+package com.rocs.chaosojbackendjudgeservice.controller.inner;
+
+import com.rocs.chaosojbackendjudgeservice.judge.JudgeService;
+import com.rocs.chaosojbackendmodel.model.entity.QuestionSubmit;
+import com.rocs.chaosojbackendserviceclient.service.JudgeFeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/inner")
+public class JudgeInnerController implements JudgeFeignClient {
+
+    private JudgeService judgeService;
+    @Override
+    @PostMapping("/do")
+    public QuestionSubmit doJudge(@RequestParam("questionSubmitId") long questionSubmitId) {
+        return judgeService.doJudge(questionSubmitId);
+    }
+}
---
+++ chaosoj-backend-question-service/src/main/java/com/rocs/chaosojbackendquestionservice/controller/inner/QuestionInnerController.java
@@ -0,0 +1,38 @@
+package com.rocs.chaosojbackendquestionservice.controller.inner;
+
+import com.rocs.chaosojbackendmodel.model.entity.Question;
+import com.rocs.chaosojbackendmodel.model.entity.QuestionSubmit;
+import com.rocs.chaosojbackendquestionservice.service.QuestionService;
+import com.rocs.chaosojbackendquestionservice.service.QuestionSubmitService;
+import com.rocs.chaosojbackendserviceclient.service.QuestionFeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController("/inner")
+public class QuestionInnerController implements QuestionFeignClient {
+
+    @Resource
+    private QuestionService questionService;
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    @Override
+    @GetMapping("/get/id")
+    public Question getQuestionById(@RequestParam("questionId") long questionId) {
+        return questionService.getById(questionId);
+    }
+
+    @Override
+    @GetMapping("/question_submit/get/id")
+    public QuestionSubmit getQuestionSubmitById(@RequestParam("questionSubmitId") long questionSubmitId) {
+        return questionSubmitService.getById(questionSubmitId);
+    }
+
+    @Override
+    @PostMapping("/question_submit/update")
+    public boolean updateQuestionSubmitById(@RequestBody QuestionSubmit questionSubmit) {
+        return questionSubmitService.updateById(questionSubmit);
+    }
+}
